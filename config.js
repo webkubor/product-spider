9e9e6db9a1d66ff6b9d72532b30a6344b62fda5b
webkubor <webkubor@163.com>
2025-04-08 11:26:54 +0800

    将小米商店和智能手表爬虫配置注释掉，并更新CopyPencil包包产品爬虫配置的注释

--- config.js
+++ config.js
@@ -2,35 +2,35 @@
 export const scrapingConfig = {
   
   // 小米商店爬虫配置
-  "Lifestyle": {
-    "url": "https://mistore.pk/pages/lifestyle",
-    "type": "standard",
-    "waitSelector": ".no_crop_image.grid-item",
-    "waitTime": 8000,
-    "autoScroll": true,
-    "selectors": {
-      "product": ".no_crop_image.grid-item",
-      "name": ".product-title",
-      "price": "p.regular-product>span",
-      "image": "img"
-    }
-  },
+  // "Lifestyle": {
+  //   "url": "https://mistore.pk/pages/lifestyle",
+  //   "type": "standard",
+  //   "waitSelector": ".no_crop_image.grid-item",
+  //   "waitTime": 8000,
+  //   "autoScroll": true,
+  //   "selectors": {
+  //     "product": ".no_crop_image.grid-item",
+  //     "name": ".product-title",
+  //     "price": "p.regular-product>span",
+  //     "image": "img"
+  //   }
+  // },
   
   // 智能手表爬虫配置
-  "xiaomi-watches": {
-    "url": "https://xcessorieshub.com/product-category/mobile-xcessories/smart-watches/xiaomi-smart-watches/",
-    "type": "standard",
-    "waitSelector": "ul.products",
-    "selectors": {
-      "product": "ul.products li.product",
-      "name": "h2.woocommerce-loop-product__title",
-      "price": ".price",
-      "image": "img",
-      "link": "a.woocommerce-LoopProduct-link"
-    }
-  },
+  // "xiaomi-watches": {
+  //   "url": "https://xcessorieshub.com/product-category/mobile-xcessories/smart-watches/xiaomi-smart-watches/",
+  //   "type": "standard",
+  //   "waitSelector": "ul.products",
+  //   "selectors": {
+  //     "product": "ul.products li.product",
+  //     "name": "h2.woocommerce-loop-product__title",
+  //     "price": ".price",
+  //     "image": "img",
+  //     "link": "a.woocommerce-LoopProduct-link"
+  //   }
+  // },
   
-  // CopyPencil 包包爬虫配置
+  // CopyPencil 包包产品爬虫配置
   "copypencil-bags": {
     "url": "https://copypencil.pk/collections/bags-amp-pouches-view-all-bags",
     "type": "standard",
@@ -46,11 +46,11 @@ export const scrapingConfig = {
   },
   
   // DJI 图片爬虫配置
-  "dji-images": {
-    "url": "https://www.dji.com/cn/camera-drones?site=brandsite&from=nav",
-    "type": "image",
-    "waitForNetworkIdle": true
-  }
+  // "dji-images": {
+  //   "url": "https://www.dji.com/cn/camera-drones?site=brandsite&from=nav",
+  //   "type": "image",
+  //   "waitForNetworkIdle": true
+  // }
 };
 
 // 可以根据需要添加更多站点配置
